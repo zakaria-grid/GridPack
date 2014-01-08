dcda46268fdb0626c3d6b4f5cf2c8df686fbcfc4
billp <billp>
2014-01-08 16:38:30 +0000

    one-library: merge changes from trunk back to r490

--- math/petsc/math.cpp
+++ math/petsc/math.cpp
@@ -32,13 +32,37 @@ Initialize(void)
 {
   if (Initialized()) return;
   PetscErrorCode ierr(0);
+  PetscBool flg;
   try {
+// Turn this on to enable PETSc logging.
+#if 0
+    int argc = 2;
+    char **args;
+    args = new char*[2];
+    args[0] = new char[32];
+    args[1] = new char[32];
+    sprintf(args[0],"powerflow.x");
+    sprintf(args[1],"-log_summary");
+    ierr = PetscInitialize(&argc,&args,NULL,NULL); CHKERRXX(ierr);
+    delete [] args[1];
+    delete [] args[0];
+    delete [] args;
+#else
     ierr = PetscInitializeNoArguments(); CHKERRXX(ierr);
+#endif
+    PetscOptionsHasName(NULL,"-log_summary",&flg);
     ierr = PetscOptionsInsertFile(PETSC_COMM_WORLD,
                                   "gridpack.petscrc",
                                   PETSC_FALSE); CHKERRXX(ierr);
   } catch (const PETSc::Exception& e) {
     throw PETScException(ierr, e);
+  }
+  // Print out some information on processor configuration
+  int mpi_err, me, nproc;
+  mpi_err = MPI_Comm_rank(PETSC_COMM_WORLD, &me);
+  mpi_err = MPI_Comm_size(PETSC_COMM_WORLD, &nproc);
+  if (me == 0) {
+    printf("\nGridPACK math module configured on %d processors\n",nproc);
   }
 }
 
